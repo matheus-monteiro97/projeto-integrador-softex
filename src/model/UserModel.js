2825eb9bc187d60b0c49d45efc3611a69565df1f
Daniela Xavier <danielamxsoares@gmail.com>
2023-12-28 16:27:56 -0300

    feat: tornar UserModel uma classe abstrata e extender para CustomerModel

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -1,34 +1,46 @@
 const Sequelize = require("sequelize");
 const database = require("../config/dbConnection");
 
-const UserModel = database.define("user", {
-  name: {
-    type: Sequelize.STRING(100),
-    allowNull: false,
-  },
-  phoneNumber: {
-    type: Sequelize.STRING(14),
-    unique: true,
-    allowNull: false,
-  },
-  emailAddress: {
-    type: Sequelize.STRING(255),
-    unique: true,
-    allowNull: false,
-  },
-  password: {
-    type: Sequelize.STRING(60),
-    unique: true,
-    allowNull: false,
-  },
-  dateRegistration: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
-  userType: {
-    type: Sequelize.ENUM("Customer", "Employee"),
-    allowNull: false,
-  },
-});
+class ClasseAbstrata {
+  constructor() {
+    if (this.constructor === ClasseAbstrata) {
+      throw new Error("Essa classe não pode ser instanciada!");
+    }
+  }
+}
+class UserModel extends ClasseAbstrata {
+  constructor() {
+    super();
+    this.user = database.define("user", {
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      phoneNumber: {
+        type: Sequelize.STRING(14),
+        unique: true,
+        allowNull: false,
+      },
+      emailAddress: {
+        type: Sequelize.STRING(255),
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(60),
+        unique: true,
+        allowNull: false,
+      },
+      dateRegistration: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      userType: {
+        type: Sequelize.ENUM("Customer", "Employee"),
+        allowNull: false,
+      },
+    });
+  }
+}
 
 module.exports = UserModel;
