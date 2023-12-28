9e896a32edec8c5ce28e8f8edd7d2b08e5e2efff
Daniela Xavier <danielamxsoares@gmail.com>
2023-12-28 17:06:54 -0300

    feat: Criar CalledModel

---
+++ src/model/calledModel.js
@@ -0,0 +1,65 @@
+const Sequelize = require("sequelize");
+const database = require("../config/dbConnection");
+const custumer = require("./CustomerModel");
+
+class CalledModel {
+  constructor() {
+    this.called = database.define("called", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      idCustomer: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: custumer,
+          key: "id",
+        },
+      },
+      creationDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      closingDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      titleCalled: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      problem: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      solution: {
+        type: Sequelize.STRING(500),
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING(500),
+        allowNull: false,
+      },
+      priority: {
+        type: Sequelize.ENUM("high", "mean", "low"),
+        allowNull: false,
+      },
+      statusCalled: {
+        type: Sequelize.ENUM("open", "progress", "close"),
+        allowNull: false,
+      },
+      isActive: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+    });
+
+    colled.belongsTo(custumer, {
+      foreignKey: "idCustomer",
+    });
+  }
+}
+
+module.exports = CalledModel;
