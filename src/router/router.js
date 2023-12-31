8f4865cd31878c162c3ca78d98d0ddb180f23432
matheus-monteiro97 <matheusmonteirolima1997@gmail.com>
2023-12-31 18:11:08 -0300

    Fix: Importações necessárias no arquivo router

---
+++ src/router/router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const calledController = require("../controller/calledController");
+const customerController = require("../controller/customerController");
+const employeeController = require("../controller/employeeController");
+const userController = require("../controller/UserController");
+
+module.exports = router;
