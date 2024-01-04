b3539bb0599b30b29670df377953784e1b11e882
matheus-monteiro97 <matheusmonteirolima1997@gmail.com>
2024-01-04 19:39:11 -0300

    Fix: Reorganização do arquivo de rotas

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const calledController = require("../controller/calledController");
-const customerController = require("../controller/customerController");
-const userController = require("../controller/UserController");
 const EmployeeController = require("../controller/employeeController");
 const employeeController = new EmployeeController
-router.use(express.json());
-// CRUD EMPLOYEE
+const bodyParser = require("body-parser");
+
+
+router.use(bodyParser.json());
+
+// CRUD EMPLOYEE e USER
 router.post("/employee", employeeController.createEmployee);
+
+  
 
 module.exports = router;
