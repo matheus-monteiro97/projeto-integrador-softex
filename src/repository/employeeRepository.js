8f67d06bff0a101d30ae1aa7a02cd9f4033975fa
matheus-monteiro97 <matheusmonteirolima1997@gmail.com>
2024-01-05 17:31:26 -0300

    Feat: Criação do método getAll em employeeRepository

--- src/repository/employeeRepository.js
+++ src/repository/employeeRepository.js
@@ -1,6 +1,9 @@
 const EmployeeModel = require('../model/employeeModel');
+const UserModel = require("../model/UserModel");
 
 class EmployeeRepository {
+
+    // FUNÇÃO PARA CRIAR FUNCIONÁRIOS
     static createEmployee = async function (data) {
         try {
             const newEmployee = await EmployeeModel.employee.create(data);
@@ -10,6 +13,24 @@ class EmployeeRepository {
             throw new Error("Error creating Employee");
         }
     };
+
+    //FUNÇÃO PARA OBTER TODOS OS FUNCIONÁRIOS
+    static async getAllEmployees() {
+        try {
+            const employees = await EmployeeModel.employee.findAll({
+                include: [{
+                model: UserModel.user,
+                required: true,
+                where: { userType: 'Employee' }, //CONDIÇÃO PARA RESGATAR USER E EMPLOYEE
+            }],
+        });
+            return employees;
+        } catch (error) {
+            console.log(error.message)
+            throw new Error("Error retrieving all employees:" , error.message);
+        }
+    }
+
 }
 
 module.exports = EmployeeRepository;
