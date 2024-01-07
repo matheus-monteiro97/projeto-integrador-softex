f841cbbfe2bd8e9a747893e2c09b2655737b5e21
matheus-monteiro97 <matheusmonteirolima1997@gmail.com>
2024-01-06 22:39:59 -0300

    Feat: Criação de rotas para getById de customer e employee

--- src/router/router.js
+++ src/router/router.js
@@ -13,9 +13,11 @@ router.use(bodyParser.json());
 // CRUD EMPLOYEE e USER
 router.post("/employee", employeeController.createEmployee);
 router.get("/employee", employeeController.getAllEmployees);
+router.get("/employee/:id", employeeController.getByIdEmployee);
 
 // CRUD CUSTUMER e USER
 router.post("/customer", customerController.createCustumer);
 router.get("/customer", customerController.getAllCustomers);
+router.get("/customer/:id", customerController.getByIdCustomer);
 
 module.exports = router;
