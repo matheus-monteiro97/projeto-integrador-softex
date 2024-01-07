e1dcc0b0e4d125b87ee458dfd38e97e2d75774b1
matheus-monteiro97 <matheusmonteirolima1997@gmail.com>
2024-01-06 23:14:01 -0300

    Feat: Criação de método delete em employeeRepository

--- src/repository/employeeRepository.js
+++ src/repository/employeeRepository.js
@@ -1,5 +1,6 @@
 const employeeModel = require('../model/employeeModel');
 const userModel = require("../model/UserModel");
+const database = require("../../config/connectionDB");
 
 class EmployeeRepository {
     static createEmployee = async function (data) {
@@ -43,6 +44,40 @@ class EmployeeRepository {
         } catch (error) {
             console.error('Error while fetching the Employee:', error.message);
             throw new Error('Error while fetching the Employee');
+        }
+    }
+
+    static async deleteEmployee(id) {
+        const transaction = await database.transaction();
+
+        try {
+            const employee = await employeeModel.employee.findOne({
+                where: { id },
+                include: [userModel.user],
+                transaction,
+            });
+
+            if (!employee) {
+                throw new Error('Employee not found for the provided ID');
+            }
+
+            // Excluir o Employee
+            await employee.destroy({ transaction });
+
+            // Excluir o User associado
+            const user = await userModel.user.findByPk(employee.userId, { transaction });
+            await user.destroy({ transaction });
+
+            // Commit da transação
+            await transaction.commit();
+
+            return employee;
+        } catch (error) {
+            // Rollback em caso de erro
+            await transaction.rollback();
+
+            console.error('Error while deleting the Employee and associated User:', error.message);
+            throw new Error('Error while deleting the Employee and associated User');
         }
     }
 }
