afabd8caa0f1fd37f4a96fc1344ec75a08eabd0b
matheus-monteiro97 <matheusmonteirolima1997@gmail.com>
2024-01-08 14:53:46 -0300

    Fix: Retirada de body-parser do app.js, pois, já está em router

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const router = require("./src/router/router");
 const database = require("./config/connectionDB");
 const employee = require("./src/model/employeeModel");
 const customer = require("./src/model/customerModel");
 const called = require("./src/model/calledModel");
-app.use(bodyParser.json());
 
 app.use("/", router);
 
