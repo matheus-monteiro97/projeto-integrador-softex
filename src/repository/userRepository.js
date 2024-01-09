15e8fbbac5c2b9b66c3ec52534ab262361d6a225
matheus-monteiro97 <matheusmonteirolima1997@gmail.com>
2024-01-09 18:55:12 -0300

    Feat: Criação de login com jwt

--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -1,4 +1,7 @@
-const userModel = require("../model/UserModel");
+const userModel = require("../model/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
 class UserRepository {
   static createUser = async function (data) {
     try {
@@ -9,6 +12,31 @@ class UserRepository {
       // throw new Error("Error creating User");
     }
   };
+
+  static async login(emailAddress, password) {
+    try {
+      // Buscar o usuário pelo endereço de e-mail
+      const user = await userModel.user.findOne({ where: { emailAddress } });
+
+      // Verificar se o usuário existe
+      if (!user) {
+        throw new Error('Usuário não encontrado');
+      }
+
+      // Verificar se a senha está correta
+      const passwordValid = await bcrypt.compare(password, user.password);
+      if (!passwordValid) {
+        throw new Error('Senha incorreta');
+      }
+
+      // Criar um token JWT
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+      return { user, token };
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 module.exports = UserRepository;
