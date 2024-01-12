eb2df957526520ac405123c0be6fecc158e47174
matheus-monteiro97 <matheusmonteirolima1997@gmail.com>
2024-01-12 10:13:07 -0300

    Fix: Correção de validações de senha e número

--- src/middleware/registrationMiddleware.js
+++ src/middleware/registrationMiddleware.js
@@ -15,13 +15,13 @@ class RegistrationMiddleware {
     
         const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
 
-        if (password && password.length < 8 && !passwordRegex.test(password)) {
+        if (password && password.length < 8 || !passwordRegex.test(password)) {
           return res.status(400).json({
             error: "Invalid password. It should contain at least one lowercase letter, one uppercase letter, one number, and one special character."
           });
         }
 
-        if (phoneNumber && !validator.isNumeric(phoneNumber) && phoneNumber.length !== 11) {
+        if (phoneNumber && !validator.isNumeric(phoneNumber) || phoneNumber.length !== 11) {
             return res.status(400).json({ error: "Invalid phone number. It should have exactly 11 digits." });
         }
       
